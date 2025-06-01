ab596f1f64c53495bb9ff09c8504b5e3858570aa
Tolai <tolyars@mail.ru>
2025-06-01 22:49:38 +0700

    Lesson4.2

--- lessons.js
+++ lessons.js
@@ -479,36 +479,333 @@ function compareNum2(a,b) {
 console.log(arrray);
 
 console.log('Урок 2.19 - Динамическая типизация в JS');
-let a;
-console.log(a); // получим undefined , null - это когда переменная не была даже объявлена
-console.log('TO STRING');
-console.log(typeof(String(4)));
-console.log(typeof(4 + '5'));
-console.log('red' + false);
-console.log('TO NUMBER');
-console.log(typeof(Number(4)));
-console.log(typeof(+'5'));
-console.log(5 + +'5');
-console.log(typeof(parseInt('15px', 10)));
-console.log(typeof('blue' / 10)); console.log('blue' / 10);
-console.log('TO BOOLEAN');
-console.log(typeof(Boolean('red')));
-console.log(typeof(!!'5'));
-let x = 5;
-console.log(++x);
-console.log([] + false - null + true);
-let y = 1;
-let x = y = 2;
-console.log(y);
-console.log([] + 1 + 2);
-console.log('1'[0]);
-console.log(2 && 1 && 5 && 3);
-let a = '2'; let b = '';
-console.log(!!( a && b ));
-console.log( a && b );
-console.log(null || 4 && 3 || null );
-a = [1, 2, 3]; b = [1, 2, 3];
-console.log(a == b);
-console.log(+"Infinity");
-console.log('е'>'Ж');
-console.log(0 || "" || 2 || undefined || true || falsе);
+// let a;
+// console.log(a); // получим undefined , null - это когда переменная не была даже объявлена
+// console.log('TO STRING');
+// console.log(typeof(String(4)));
+// console.log(typeof(4 + '5'));
+// console.log('red' + false);
+// console.log('TO NUMBER');
+// console.log(typeof(Number(4)));
+// console.log(typeof(+'5'));
+// console.log(5 + +'5');
+// console.log(typeof(parseInt('15px', 10)));
+// console.log(typeof('blue' / 10)); console.log('blue' / 10);
+// console.log('TO BOOLEAN');
+// console.log(typeof(Boolean('red')));
+// console.log(typeof(!!'5'));
+// let x = 5;
+// console.log(++x);
+// console.log([] + false - null + true);
+// console.log([] + false); // пустой массив при сложении превращается в строку, соответственно второе слагаемое, также превращается в строку
+// let y = 1;
+// let x = y = 2;
+// console.log(y);
+// console.log([] + 1 + 2);
+// console.log('1'[0]);
+// console.log(2 && 1 && 5 && 3);
+// let a = '2'; let b = '';
+// console.log(!!( a && b ));
+// console.log( a && b );
+// console.log(null || 4 && 3 || null );
+// a = [1, 2, 3]; b = [1, 2, 3];
+// console.log(a == b);
+// console.log(+"Infinity"); //результат это число - но записанное словом
+// console.log('е'>'Ж');
+// console.log(0 || "" || 2 || undefined || true || falsе);
+// console.log('154'[1]); // из строки "154" получаем второй символ (так как [1]=2), получаем "5" 
+// console.log('абвг'[2]);
+
+console.log('Урок 2.22 - Получение элементов со страницы');
+
+console.log('Урок 2.28 - Мобильные события. Регулярные выражения');
+
+let herName = 'Carolina 1978';
+let reg =/a/ig;
+// метод search ищет нужный символ и выдает первый попавшийся, иначе -1
+console.log(herName.search(reg)); // выдаст 1
+// метод matsh ищет либо первый, либо все (g - global) символы и выдаёт результат в виде массив
+console.log(herName.match(reg)); // выдаст [ 'a', 'a' ]
+// метод replace заменяет символ
+console.log(herName.replace(/./g, '*')); // выдаст *************
+// метод test
+console.log(reg.test(herName)); // выдаст True
+
+// классы символов  \d - digits, \w - words, \s - spaces
+console.log(herName.match(/\d/g)); // выдаст [ '1', '9', '7', '8' ]
+
+let model = 'Samsung 24f5l9';
+console.log(model.match(/\d\d\w\d\w\d/i)); // выдаст [ '24f5l9', index: 8, input: 'Samsung 24f5l9', groups: undefined ]
+
+console.log('Урок 3.01 - Скрипты и время их выполнения. setTimeout и setInterval');
+// setTimeout(func, 3000);
+// function func() {
+//     console.log("3 сек прошло")
+// }
+
+// let timerId = setTimeout(func2, 4000);
+// function func2() {
+//     console.log('Приветик')
+// };
+// clearTimeout(timerId);
+
+// let timerId2 = setInterval(func3, 3500);
+// function func3() {
+//     console.log('1,2,3...')
+// }
+
+// // Рекурсивный вызов
+// let timerId3 = setTimeout(function log() {
+//     console.log('Привет');
+//     setTimeout(log, 2000)
+// })
+
+console.log('Урок 3.5');
+// let p = 45;
+// let o = p.toString().padStart(3,0);
+// console.log(o);
+// console.log(typeof(o));
+console.log('Урок 3.6 - Конструкторы и классы');
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello! ' + this.name);
+//     };
+// };
+
+// let ivan = new User('Ivan', 25),
+//     alex = new User('Alex', 36);
+
+// console.log(ivan);
+// console.log(alex);
+// console.log(alex.hello() + " " + ivan.hello());
+
+// // Использование prototype для создания новых методов в нашем функции-конструкторе
+// User.prototype.exit = function() {
+//     console.log('Пользователь ' + this.name + ' ушел из чата');
+// };
+// console.log(ivan.exit());
+console.log('Урок 3.7 - This. Контекст вызова и как его использовать');
+// // 1 вариант - стандартный вызов функции
+// function showLog() {
+//     console.log(this)
+// }
+// showLog();
+
+// function showThis(a, b) {
+//     console.log(this); // даст window
+//     function sum() {
+//         console.log(this); // даст window
+//         // return this.a + this.b; // даст NaN
+//         return a + b; 
+//     }
+//     console.log(sum());
+// }
+// showThis(4,5);
+// showThis(2,3);
+
+// // 2
+// let obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//     }
+// }
+// obj.sum();
+
+// // но
+// let obj2 = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//         function shout() {
+//             console.log(this);
+//         }
+//         shout(); // выдаст window, так как функция shout теряет контекст, так как она вызвана не как метод объекта, а просто как функция внутри функции
+//     }
+// }
+// obj2.sum();
+
+// // 4 Ручное присваивание this
+// let user = {
+//     name: 'john',
+// };
+
+// function sayName() {
+//     console.log(this);
+//     console.log(this.name);
+// };
+// // насильно связываем объект и функцию (расположенные рядом выше), т. е. навязываем контекст вызова
+// // в этом нам помогут 3 метода: call и apply и bind
+// console.log(sayName.call(user));
+// console.log(sayName.apply(user));
+// // в чем же разница. Рассморим ниже пример более сложной функции - с аргументами
+// function sayName2(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// };
+// console.log(sayName.call(user, 'Peter')); // call принимает аргумент в виде строки
+// console.log(sayName.apply(user, ['Snow'])); // если нужно передать несколько аргументов. Здесь можно передать массив
+
+// //
+// function count(number) {
+//     return this * number;
+// }
+// let double = count.bind(2);
+// console.log(double(3));
+// console.log(double(10));
+console.log('Урок 3.8 - Дом задание');
+let age = '30';
+function showUser(surname, name) {
+    console.log("Пользователь " + surname + " " + name + ", его возраст " + this);
+}
+let output = showUser.apply(age, ['Tolai', 'Rongin', 'Sergeevich']);
+
+
+console.log('Урок 3.9 - ES6. Интерполяция');
+let nameI = 'Ivan',
+    ageI = 30,
+    mail = 'ex@mail.ru';
+console.log('Пользователю ' + nameI + ' ' + ageI + ' лет. Его почтовый адрес: ' + mail);
+console.log(`Пользователю ${nameI} ${ageI} лет. Его почтовый адрес: ${mail}`);
+
+// const и let
+// особенность var - в циклах не создаётся массив i
+function makeArray() {
+    var items = [];
+
+    // for (var i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
+        var item = function() {
+            console.log(i);
+        };
+        items.push(item);
+    }
+    return items;
+}
+var arrra = makeArray();
+arrra[1]();
+arrra[3]();
+arrra[7]();
+
+// стрелочные функции
+let fun = () => {
+    console.log(this);
+};
+fun();
+
+let objec = {
+    number: 5,
+    sayNumber: function() {
+        let say = () => {
+            console.log(this);
+        };
+        say();
+    }
+};
+objec.sayNumber();
+
+
+console.log('Урок 3.12 - Параметры по умолчанию');
+// старый вариант (до ES6), который не был достаточно надежен
+function calcOrDouble(number, basis) {
+    basis = basis || 2; // проверка basis на то, что этот аргумент был передан, иначе basis = 2
+    console.log(number * basis);
+}
+calcOrDouble(5, 6); // 30
+calcOrDouble(10);  // 20
+
+// новый вариант (ES6)
+function calcOrDouble2(number, basis = 2) {
+    console.log(number * basis);    
+}
+calcOrDouble2(7);  // 14
+
+console.log('Урок 3.13 - Классы');
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+const square = new Rectangle(10,10);
+console.log(square);
+console.log(square.calcArea());
+
+
+console.log('Урок 3.14 - Spread операторы');
+
+let video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [video, blogs, 'vk', 'FB'];
+console.log(internet); // выдаст массив, в котором помимо 2х строк еще 2 массива
+let internet2 = [...video, ...blogs, 'vk', 'FB'];
+console.log(internet2); // выдаст ОДИН массив со всеми данными
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(a+b+c);
+}
+let numbersTri = [2, 5, 7];
+log(numbersTri); // выдаст массив и 2 undefined 
+log(...numbersTri); // выдаст 2 5 7
+
+ class Options {
+        constructor(height, width, fontSize, textAlign) {
+            // this.height = height;
+            // this.width = width;
+            // this.backgroundColor = bg;
+            // this.fontSize = fontSize;
+            // this.textAlign = textAlign;
+        }
+        createDiv() {
+            document.body.appendChild(div);
+            this.style.height = height + 'px';
+            this.style.width = width + 'px';
+            // this.style.background-color = bg;
+            this.style.fontSize = fontSize + 'px';
+            this.style.textAlign = textAlign;            
+        }
+    };
+
+const newDiv = new Options(100, 100, 16, 'left');
+newDiv.createDiv();
+
+console.log('Урок 4.1 - JSON');
+
+let options = {
+    width: 1366,
+    height: 768,
+    background: 'red',
+    font: {
+        size: '16px',
+        color: '#FFF',
+    }
+}
+// переводим в формат JSON
+let toJSON = JSON.stringify(options); // выдаст {"width":1366,"height":768,"background":"red","font":{"size":"16px","color":"#FFF"}}
+// переводим обратно из JSON
+let fromJSON = JSON.parse(toJSON);
+console.log(toJSON);
+console.log(fromJSON);
+
+const today = new Date();
+console.log(today);
+let dayOfMonth = today.getDate();
+console.log(dayOfMonth);
+let year = today.getFullYear();
+console.log(year);
+let monthOfYear = today.getMonth()
+console.log(monthOfYear);
+let newYear = today.setFullYear(2025,6,1);
+console.log(newYear);
--- script.js
+++ script.js
@@ -1,90 +1,110 @@
-let money, time;
+let box = document.querySelector('.box'),
+    btn = document.querySelector('.btn');
 
-function start() {
-    money = +prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let width = box.offsetWidth,
+    height = box.offsetHeight;
 
-    while(isNaN(money) || money == "" || money == null) {
-        money = +prompt("Ваш бюджет на месяц?", 'Введите корректное значение');
-    };
-}
-start();
+console.log(width);
+console.log(height);
+console.log(box.getBoundingClientRect());
+console.log(box.getBoundingClientRect().left);
+console.log(document.documentElement.clientWidth); // viewport width
+console.log(document.documentElement.clientHeight); // viewport height
 
-let appData = {
-	budget: money,
-	expenses: {},
-	optionalExpenses: {},
-	income: [],
-	timeData: time,
-	savings: true,
-    chooseExpenses: function () {
-        for (let i = 0; i < 2; i++) {
-            let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-                b = prompt("Во сколько обойдется?", '');
-            // а далее внесем код, который не пропустит пустые поля при заполнении пользователем
-            if ((typeof(a)) === 'string' // ответ должен быть строкой
-                && (typeof(a)) != null // ответ не должен быть null
-                && (typeof(b)) != null // ответ не должен быть null
-                && a != '' // ответ не должен быть пустым
-                && b != '' // ответ не должен быть пустым
-                && a.length < 50 ) // в ответе не более 50 символов
-            {
-                console.log('done');
-                appData.expenses[a] = b;
-            } else {i--};
-        };
-    },
-    detectDayBudget: function() {
-        appData.moneyPerDay = +(appData.budget / 30).toFixed();    
-        alert('Ежедневный бюджет: ' + appData.moneyPerDay +" руб.");
-    },
-    detectLevel: function() {
-        if (appData.moneyPerDay < 500) {
-            console.log('Минимальный уровень достатка')
-        } else if (appData.moneyPerDay > 500 && appData.moneyPerDay < 2000) {
-            console.log('Средний уровень достатка')
-        } else if (appData.moneyPerDay > 2000 && appData.moneyPerDay < 5000) {
-            console.log('Достойный уровень достатка')
-        } else if (appData.moneyPerDay > 5000) {
-            console.log('Хорошо живём!)')
-        } else { console.log('Не понятно')};
-    },
-    checkSavings: function() {
-        if (appData.savings == true) {
-            let save = +prompt('Какова сумма накоплений?'),
-                percent = +prompt('Под какой процент?');
-    
-            appData.monthIncome = +save/100/12*percent;
-            alert("Доход в месяц с Вашего депозита: " + appData.monthIncome);
-        }
-    },
-    chooseOptionalExpenses: function() {
-        for (let i = 0; i < 3; i++) {
-            let questionOptExpenses = prompt("Статья необязательных расходов?", '');
-            appData.optionalExpenses[i] = questionOptExpenses;
-            console.log(appData.optionalExpenses);
-        };
-    },
-    chooseIncome: function() {
-        let items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)','');
-        if ((typeof(items)) === 'string' // ответ должен быть строкой
-        && (typeof(items)) != null // ответ не должен быть null
-        && items != '' // ответ не должен быть пустым
-        && items.length < 50 ) {
-        appData.income = items.split(', ');
-        appData.income.push(prompt('Может что-то еще?',''));
-        appData.income.sort();
-        appData.income.forEach(function(item, i) {
-            console.log((i+1) + ' Способ доп. заработка: ' + item);
-        });
-        }
-        else {
-            alert('Неверно введено, введите заново');
-        }
+document.documentElement.scrollTop = 0; // возвращает viewport в начало
+
+// btn.addEventListener('click', function() {
+//     box.style.height = box.scrollHeight + 'px';
+//     console.log(box.getBoundingClientRect());
+// })
+
+
+
+// 'use strict';
+// function showLog() {
+//     console.log(this)
+// }
+// showLog();
+
+// function showThis(a, b) {
+//     console.log(this); // даст window
+//     function sum() {
+//         console.log(this); // даст window
+//         // return this.a + this.b; // даст NaN
+//         return a + b; 
+//     }
+//     console.log(sum());
+// }
+// showThis(4,5);
+// showThis(2,3);
+
+
+btn.addEventListener('click', function() {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+    function showThis() {
+        console.log(this);
     }
-};
+    showThis();
+})
 
-for (let key in appData) {
-    console.log(key + 'Наша программа включает в себя данные: ' + appData[key])
-};
+// ДЗ 3.8
+let age = document.getElementById('age');
+function showUser(surname, name) {
+    console.log("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+console.log(showUser.apply(age, ['Tolai', 'Rongin', 'Sergeevich']));
 
+// стрелочная функция не имеет своего контекста вызова, её контекст вызова это родительский
+btn.addEventListener('click', function() {
+    let show = () => {
+        console.log(this);
+    }
+    show();
+})
+
+class Options {
+        constructor(height, width, bg, fontSize, textAlign) {
+            this.height = height + 'px';
+            this.width = width + 'px';
+            this.backgroundColor = bg;
+            this.fontSize = fontSize + 'px';
+            this.textAlign = textAlign;
+            console.log(this);
+        }
+        createDiv() {
+            let elem = document.createElement('div');
+            document.body.appendChild(elem);
+            elem.style.cssText = `height: ${this.height}; width: ${this.width}; background-Color: ${this.backgroundColor}; font-Size: ${this.fontSize}; text-align: ${this.textAlign}`;
+            // this.style.height = height + 'px';
+            // this.style.width = width + 'px';
+            // this.style.backgroundColor = bg;
+            // this.style.fontSize = fontSize + 'px';
+            // this.style.textAlign = textAlign;            
+        }
+    };
+
+const newDiv = new Options(100, 100, 'yellow', 16, 'left');
+newDiv.createDiv();
+
+let rub = document.querySelector('.rub'),
+    usd = document.querySelector('.usd');
+
+rub.addEventListener('input', () =>{
+    let request = new XMLHttpRequest();
+    // request.open(methos, url, async, login, password);
+    request.open('GET', 'current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    // request.send(body); когда отправляем информацию
+    request.send(); //отправляем запрос за ответом к серверу
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4 && request.status == 200) {
+            let data = JSON.parse(request.response);
+
+            usd.value = rub.value / data.usd;
+        } else {
+            usd.value = 'Что-то пошло не так!';
+        }
+    });
+});
